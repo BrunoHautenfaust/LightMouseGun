6dbe2becdd572e8bda86189f2b812b6b45c58704
BrunoHautenfaust <t1m2n3t4@yahoo.com>
2019-04-03 23:18:47 +0300

    Remove: package; Add: font size to GUI for consistency with the Windows layout

--- src/GUI.py
+++ src/GUI.py
@@ -5,6 +5,7 @@ class GUI:
         self.buttonColor = '#000'
         self.buttonBGColor = '#F5F5F5'
         self.elSize = (213, 156)
+        self.buttonSize = (3,1)
         sg.SetOptions(icon='favicon', font=('default', 10), button_color=(self.buttonColor, self.buttonBGColor))
         self.window = self.__build_UI(title)
         self.print = sg.EasyPrint
@@ -12,12 +13,12 @@ class GUI:
     def __build_UI(self, title):
         layout = [
             [sg.Frame('Position', [
-                 [sg.Button('◤', key='top_left'), sg.Button('▲', key='top_center'), sg.Button('◥', key='top_right')],
-                 [sg.Button('◀', key='middle_left'), sg.Button('◆', key='middle_center'), sg.Button('▶', key='middle_right')],
-                 [sg.Button('◣', key='bottom_left'), sg.Button('▼', key='bottom_center'), sg.Button('◢', key='bottom_right')],
+                 [sg.Button('◤', key='top_left', size=self.buttonSize), sg.Button('▲', key='top_center', size=self.buttonSize), sg.Button('◥', key='top_right', size=self.buttonSize)],
+                 [sg.Button('◀', key='middle_left', size=self.buttonSize), sg.Button('◆', key='middle_center', size=self.buttonSize), sg.Button('▶', key='middle_right', size=self.buttonSize)],
+                 [sg.Button('◣', key='bottom_left', size=self.buttonSize), sg.Button('▼', key='bottom_center', size=self.buttonSize), sg.Button('◢', key='bottom_right', size=self.buttonSize)],
                  [sg.Text('')],
                  [sg.Text('')],
-                 [sg.Button('Start', key='START'), sg.Text(''), sg.Button('Stop', key='STOP', disabled=True)]
+                 [sg.Button('Start', key='START'), sg.Text(' ' * 9), sg.Button('Stop', key='STOP', disabled=True)]
                 ], key='pos_frame'),
              sg.Frame('Preview', [
                  [sg.Image(filename='', size=self.elSize, key='OUTPUT')],
